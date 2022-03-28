845f095bbb7f8e4ba3ea9399bf27f2ea82aac0f8
German Gamarra <german199752@gmail.com>
2022-03-28 20:27:05 +0200

    Proyect_Vac version 1.0

--- src/Bienvenida.java
+++ src/Bienvenida.java
@@ -9,27 +9,20 @@ public class Bienvenida extends JFrame implements ActionListener {
     private JLabel titulo, subtitulo, etiqueta1, etiqueta2;
     private JTextField field1;
     private JButton boton1;
-
-    public static String nombreUsu = "";
+    public static String nombreUsu ="";
     public static String nombreDef ="";
 
     public Bienvenida(){
         //Diseño de pantalla
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);// El proceso se termina al cerrar la ventana
-        getContentPane().setBackground(new Color(255,0,0));//color de fondo
-
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        getContentPane().setBackground(new Color(255,0,0));
         //Diseño del titulo, imagen icono, background color.
-
-        setTitle("Bienvenido");//titulo
-        ImageIcon imagenx = new ImageIcon("C:\\Users\\German Gamarra\\OneDrive\\Escritorio\\Curso " +
-                "EOI\\Coca_Cola\\src\\images\\icon.png");
-        setIconImage(imagenx.getImage());//El icono del titulo
+        setTitle("Bienvenido");
+        ImageIcon imagenx = new ImageIcon(".\\src\\images\\icon.png");
+        setIconImage(imagenx.getImage());
 
         //El label del TITULO.
-        ImageIcon imagen = new ImageIcon("C:\\Users\\German Gamarra\\OneDrive\\Escritorio\\Curso EOI\\Coca_Cola\\src\\images\\logo-coca.png");
-        //ImageIcon imagen2 = new ImageIcon(imagen.getImage().getScaledInstance(300,90,Image.SCALE_SMOOTH));
-        // Redimensionar una imagen pero no hace que se vea mejor
+        ImageIcon imagen = new ImageIcon(".\\src\\images\\logo-coca.png");
         titulo = new JLabel(imagen);
         titulo.setBounds(25,15,300,150);
         add(titulo);
@@ -39,7 +32,6 @@ public class Bienvenida extends JFrame implements ActionListener {
         subtitulo.setBounds(50,160,300,40);
         subtitulo.setFont(new Font("Arial",3,18));
         subtitulo.setForeground(new Color(255,255,255));
-        //Font font: 0 normal, 1 negrita, 2 cursiva, 3 negrita y cursiva.
         add(subtitulo);
 
         //label de solicitud del nombre
@@ -67,22 +59,19 @@ public class Bienvenida extends JFrame implements ActionListener {
         //boton1.setEnabled(false);
 
         //texto de copyright
-        etiqueta2 = new JLabel("© 2017 The Coca-Cola Company");
+        etiqueta2 = new JLabel("© 2022 The Coca-Cola Company");
         etiqueta2.setBounds(85,375,300,30);
         etiqueta2.setFont(new Font("Arial",1,12));
         etiqueta2.setForeground(new Color(255,255,255));
         add(etiqueta2);
-
-
     }
     //IMPLEMENTAMOS LA ACCIÓN
     public void actionPerformed (ActionEvent e){
         if (e.getSource() == boton1){
+            //obtener nombre y corregir el formate del nombre
             nombreUsu = field1.getText().trim();
             nombreDef = nombreUsu.substring(0,1).toUpperCase(Locale.ROOT) + nombreUsu.substring(1).toLowerCase(Locale.ROOT);
 
-            //obtengo elnombre
-            //el metodo.trim() elimina los espacios en blanco
             if (nombreUsu.equals("")){
                 JOptionPane.showMessageDialog(null,"Debes ingresar tu nombre");
             }else{
@@ -99,11 +88,12 @@ public class Bienvenida extends JFrame implements ActionListener {
         }
     }
     public static void main(String[] args) {
+        //Pantalla del main
         Bienvenida pantalla = new Bienvenida();
-        pantalla.setLayout(null); //Layout absoluto
-        pantalla.setBounds(0,0,350,450);//tamaño de pantalla
-        pantalla.setResizable(false);//no redimensionable
-        pantalla.setLocationRelativeTo(null); // locación central
-        pantalla.setVisible(true);//visible, es lo último que debe colocarse para que sea todo el diseño visible.
+        pantalla.setLayout(null);
+        pantalla.setBounds(0,0,350,450);
+        pantalla.setResizable(false);
+        pantalla.setLocationRelativeTo(null);
+        pantalla.setVisible(true);
     }
 }
